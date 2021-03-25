bd4b814c2ae5ba326663e45efe37fd5a8d8c0d0d
Artem Kulakov <kulakov22@yandex.ru>
2021-03-25 10:23:06 +0700

    feat: create Main component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
 import Preloader from '../Preloader/Preloader';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import Main from '../Main/Main';
 
 function App() {
   return (
-    <div className="App">
-      <Preloader />
+    <div className='App'>
+      <Switch>
+        <Route path="/">
+          <Main/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,7 @@
+function Main(props) {
+  return (
+    <div></div>
+  );
+}
+
+export default Main;
